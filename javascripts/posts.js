ca54d7f4066d5d1a32164641bf9ecdfc4ac42a65
promethee <lunette.francois@gmail.com>
2015-10-17 11:24:18 +0200

    Update posts.js

--- javascripts/posts.js
+++ javascripts/posts.js
@@ -9,12 +9,11 @@
     xhr.onreadystatechange = function() {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
-              var post_html = JSON.parse(xhr.responseText);
+              document.getElementsByTagName('section')[0].innerHTML += xhr.responseText;
             }
         }
     };
     xhr.open("GET", '_posts/'+filename, true);
     xhr.send();
-    document.getElementsByTagName('section')[0].innerHTML += post_html;
   });
 }());
