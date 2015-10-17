709a856a8f64b02c664a0fb838bf78e68bc8fa78
promethee <lunette.francois@gmail.com>
2015-10-17 11:12:37 +0200

    Update posts.js

--- javascripts/posts.js
+++ javascripts/posts.js
@@ -1,11 +1,10 @@
 (function () {
-  if (undefined === posts) {
-    console.log("well, this is embarassing : I can't find the posts");
-    return false;
-  }
+  var posts = [
+    "2015-10-17-hello.md"
+  ];
 
   document.getElementsByTagName('section')[0].textContent = '';
-  /*posts.map(function (post_html) {
+  posts.map(function (post_html) {
     document.getElementsByTagName('section')[0].innerHTML += post_html;
-  });*/
+  });
 });
