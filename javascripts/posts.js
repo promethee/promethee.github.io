a991bbe180cf9e57c73b9f4ac0e88b144f1e2f60
promethee <lunette.francois@gmail.com>
2015-10-17 15:14:24 +0200

    Update posts.js

--- javascripts/posts.js
+++ javascripts/posts.js
@@ -1,5 +1,24 @@
-window.posts = [
-    '5edd82669f0a87faea90',
-    'a99d59b392e569c03a37',
-    '8648582c70bb79b97044',
-];
+$.get(
+    'https://api.github.com/users/promethee/gists',
+    function (gists) {
+        gists.map(function (gist) {
+            var keys = Object.keys(gist.files);
+            keys.map(function (k) {
+                $.get(
+                    gist.files[keys].raw_url,
+                    function (data) {
+                        console.log('data', data);
+                        var p_container = document.createElement('p');
+                        p_container.dataset.gistId = gist.id;
+                        p_container.style.display = 'block';
+
+                        var div_gist = document.createElement('div');
+                        div_gist.className = 'gist';
+                        div_gist.innerHTML = markdown.toHTML(data);
+                        p_container.appendChild(div_gist);
+                        document.getElementsByTagName('section')[0].appendChild(p_container);
+                    });
+            });
+        });
+    }
+);
