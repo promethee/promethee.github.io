ed1e430687f5dd6dbf95ed5000b1af3dc67b604e
promethee <lunette.francois@gmail.com>
2015-10-23 14:21:48 +0200

    Update posts.js

--- javascripts/posts.js
+++ javascripts/posts.js
@@ -4,10 +4,28 @@ var addGist = function (gist) {
     var p_container = document.createElement('p');
     p_container.dataset.gistId = gist.id;
     p_container.style.display = 'block';
+
     var div_gist = document.createElement('div');
     div_gist.className = 'gist';
     div_gist.dataset.description = gist.description || '';
-    div_gist.innerHTML = markdown.toHTML(gist.data);
+    // div_gist.innerHTML = markdown.toHTML(gist.data);
+
+    var div_gist_date = document.createElement('small');
+    div_gist_date.className = 'gitst-date'
+    div_gist_date.innerHTML = moment(gist.created_at).format('dddd MMMM Do YYYY');
+
+    var div_gist_content = document.createElement('p');
+    div_gist_content.className = 'gist-content';
+    div_gist_content.innerHTML = markdown.toHTML(gist.data);
+
+    var div_gist_tags = document.createElement('small');
+    div_gist_tags.className = 'gist-tags';
+    div_gist_tags.innerHTML = gist.description.split(' ').join(' ');
+    console.log(gist);
+
+    div_gist.appendChild(div_gist_date);
+    div_gist.appendChild(div_gist_content);
+    div_gist.appendChild(div_gist_tags);
     p_container.appendChild(div_gist);
     document.getElementsByTagName('section')[0].appendChild(p_container);
 };
@@ -21,21 +39,19 @@ var onGistListDone = function (gists) {
         posts[gist.updated_at] = gist;
     });
 
-    var updated_dates = Object.keys(posts).sort().reverse();
-    updated_dates.map(function (update_date) {
-        Object.keys(posts[update_date].files).map(function (filename) {
-            var gist_file = posts[update_date].files[filename];
+    var creation_dates = Object.keys(posts).sort().reverse();
+    creation_dates.map(function (creation_date) {
+        Object.keys(posts[creation_date].files).map(function (filename) {
+            var gist_file = posts[creation_date].files[filename];
             getGist(gist_file.raw_url, function (data) {
-                posts[update_date].data = data;
-                addGist(posts[update_date]);
+                posts[creation_date].data = data;
+                addGist(posts[creation_date]);
             });
         });
-        return posts[update_date];
+        return posts[creation_date];
     });
 };
 
 var getGistList = function (callback) {
     $.get('https://api.github.com/users/promethee/gists', onGistListDone);
 };
-
-getGistList();
